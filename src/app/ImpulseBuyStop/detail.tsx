b4a6efb3d1a729f9380e77ff03f25c21b9fe4a12
seriyama <iwakura31@gmail.com>
2025-02-10 19:29:39 +0900

    [詳細表示] add: ペンアイコンクリックハンドラ追加

--- src/app/ImpulseBuyStop/detail.tsx
+++ src/app/ImpulseBuyStop/detail.tsx
@@ -1,8 +1,13 @@
 import { View, Text,ScrollView, StyleSheet} from 'react-native'
-import Icon from '../../components/icon'
+import { router } from 'expo-router'
 
 import Header from '../../components/Header'
 import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/icon'
+
+const handlePress = (): void => {
+    router.push('ImpulseBuyStop/edit')
+}
 
 const Detail = () => {
     return (
@@ -18,7 +23,7 @@ const Detail = () => {
                     これがテストです。
                 </Text>
             </ScrollView>
-            <CircleButton style={{top:160,bottom: 'auto'}}>
+            <CircleButton onPress={handlePress} style={{top:160,bottom: 'auto'}}>
                 <Icon name='pencil' size={40} color='#FFFFFF' />
             </CircleButton>
         </View>
