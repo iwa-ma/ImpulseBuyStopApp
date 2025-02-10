299a09106938600e4d3cad3a9e63db055b43c4b2
seriyama <iwakura31@gmail.com>
2025-02-10 20:28:33 +0900

    [共通] add: ヘッダーにログアウトボタン追加

--- src/app/ImpulseBuyStop/list.tsx
+++ src/app/ImpulseBuyStop/list.tsx
@@ -1,15 +1,24 @@
-import { View, StyleSheet} from 'react-native'
-import { router } from 'expo-router'
+import { View, StyleSheet } from 'react-native'
+import { router, useNavigation } from 'expo-router'
+import { useEffect } from 'react'
 
 import BuyListItem from '../../components/BuyListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import LogOutButton from '../../components/LogoutButton'
 
 const handlePress = (): void => {
     router.push('/ImpulseBuyStop/create')
 }
 
 const List = ():JSX.Element => {
+    const navigation = useNavigation() 
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => { return <LogOutButton />}
+        })
+    }, [])
+
     return (
         <View style={styles.container}>
             <View>
