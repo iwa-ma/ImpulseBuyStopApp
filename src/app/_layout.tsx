f8bf485bff5b68f83cee8f6786d50650d6a75e30
seriyama <iwakura31@gmail.com>
2025-02-10 19:46:32 +0900

    [共通] add : ExpoRouterのヘッダー表示に変更

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,18 @@
-import { Slot } from 'expo-router'
+import { Stack } from 'expo-router'
 
 const Layout = ():JSX.Element => {
-    return <Slot />
+    return <Stack screenOptions={{
+        headerStyle: {
+            backgroundColor: '#0E75FC'
+        },
+        headerTintColor: '#ffffff',
+        headerTitle: 'Impulse Buy Stop App',
+        headerBackTitle: 'Back',
+        headerTitleStyle: {
+            fontSize: 22,
+            fontWeight:'bold'
+        }
+    }}/>
 }
 
 export default Layout
