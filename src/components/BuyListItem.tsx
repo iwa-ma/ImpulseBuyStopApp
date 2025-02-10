b2df707eee2e59fdc149498e0632c7ecc4b14c9a
seriyama <iwakura31@gmail.com>
2025-02-10 19:31:16 +0900

    [リスト] add:リスト要素クリックハンドラ追加

--- src/components/BuyListItem.tsx
+++ src/components/BuyListItem.tsx
@@ -1,18 +1,21 @@
 import { View, Text, TouchableOpacity, StyleSheet} from 'react-native'
+import { Link } from 'expo-router'
 
 import Icon from './icon'
 
 const BuyListItem = () => {
     return (
-        <View style={styles.buyListItem}>
-            <View>
-                <Text style={styles.buyListItemTitle}>ヤフーオークション</Text>
-                <Text style={styles.buyListItemDate}>2025/2/26 2:25</Text>
-            </View> 
-            <TouchableOpacity>
-                <Icon name='delete' size={32} color='#B0B0B0' />
+        <Link href='ImpulseBuyStop/detail' asChild>
+            <TouchableOpacity style={styles.buyListItem}>
+                <View>
+                    <Text style={styles.buyListItemTitle}>ヤフーオークション</Text>
+                    <Text style={styles.buyListItemDate}>2025/2/26 2:25</Text>
+                </View> 
+                <TouchableOpacity>
+                    <Icon name='delete' size={32} color='#B0B0B0' />
+                </TouchableOpacity>
             </TouchableOpacity>
-        </View>
+        </Link>
     )
 }
 
