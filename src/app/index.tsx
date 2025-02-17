c9247ea7e0e7b02ba578c4e2b76e810488d2503d
seriyama <iwakura31@gmail.com>
2025-02-17 18:56:06 +0900

    [共通] add:ログイン状態チェック処理をindexに追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,18 @@
-import { Redirect } from 'expo-router'
+import { Redirect, router } from 'expo-router'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { auth } from '../config'
 
 const Index = ():JSX.Element => {
-    return <Redirect href='./auth/log_in' />
+  useEffect(()=>{
+    // ログイン状態をチェックしてユーザーが取得できたらリスト画面に書き換え、取得できない場合はログイン画面に遷移
+    onAuthStateChanged(auth, (user) =>{
+      if ( user !== null ) {
+        router.replace('/ImpulseBuyStop/list')
+      }
+    })
+  },[])
+  return <Redirect href='./auth/log_in' />
 }
 
 export default Index
