8158f59417230666be427dfc5250099574f4108c
seriyama <iwakura31@gmail.com>
2025-02-17 18:56:54 +0900

    [作成/編集] add: 作成処理を追加

--- src/app/ImpulseBuyStop/create.tsx
+++ src/app/ImpulseBuyStop/create.tsx
@@ -1,29 +1,59 @@
-import { 
-    View, TextInput, StyleSheet, KeyboardAvoidingView
+import {
+    View, TextInput, StyleSheet
 } from 'react-native'
 import { router } from 'expo-router'
+import { collection, addDoc, Timestamp } from 'firebase/firestore'
+import { useState } from 'react'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import { db, auth } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (bodyText: string): void => {
+  // ログイン中ユーザーが取得でない場合は処理を実行せずに終了する
+  if (!auth.currentUser) {return}
+
+  const ref = collection(db, `users/${auth.currentUser.uid}/items`)
+
+  addDoc(ref,{
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then((docRef) =>{
+      console.log('success', docRef.id)
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+    }
+    )
 }
 
 // 新規作成画面
 
 const Create = ():JSX.Element => {
-    return (
-        <KeyboardAvoidingView style={styles.container} behavior='height'>
-            <View style={styles.inputContainer}>
-                {/* multiline iOSで上揃えにする為に必要 */}
-                <TextInput multiline style={styles.input} value='' />
-            </View>
-            <CircleButton onPress={handlePress}>
-                <Icon name='check' size={40} color='#ffffff' />
-            </CircleButton>
-        </KeyboardAvoidingView>
-    )
+  const [bodyText, setbodyText] = useState('')
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+    {/* iOSで発生するキーボードと投稿ボタンの表示バグ対応の為、KeyboardAvoidingViewは修正版を使用 */}
+
+      <View style={styles.inputContainer}>
+        {/* multiline iOSで上揃えにする為に必要 */}
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(newText) => { setbodyText(newText)}}
+          autoFocus
+        />
+      </View>
+      <CircleButton onPress={() => handlePress(bodyText)}>
+        <Icon name='check' size={40} color='#ffffff' />
+      </CircleButton>
+    </KeyboardAvoidingView>
+  )
 }
 
 const styles = StyleSheet.create({
