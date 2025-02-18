a2b965fe1ff07354232484b21b20643a7afe1666
seriyama <iwakura31@gmail.com>
2025-02-18 20:55:28 +0900

    [編集] add: 編集処理を追加

--- src/app/ImpulseBuyStop/edit.tsx
+++ src/app/ImpulseBuyStop/edit.tsx
@@ -1,25 +1,64 @@
 import {
-    View, TextInput, StyleSheet, KeyboardAvoidingView
+    View, TextInput, StyleSheet,
+    Alert
 } from 'react-native'
-import { router  } from 'expo-router'
+import { router,useLocalSearchParams  } from 'expo-router'
+import { useState, useEffect } from 'react'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
+import { auth, db } from '../../config'
 
-const handlePress = (): void => {
-    router.back()
+const handlePress = (id: string, bodyText: string): void => {
+  if (!auth.currentUser) { return}
+  const ref = doc(db, `users/${auth.currentUser.uid}/items`, id)
+  setDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+    .then(() =>{
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+      Alert.alert('更新に失敗しました')
+    })
 }
 
 // 編集画面
-
 const Edit = ():JSX.Element => {
+  const id = String(useLocalSearchParams().id)
+  const [bodyText, setBodyText] = useState('')
+  useEffect(() => {
+    if(!auth.currentUser){return}
+    const ref = doc(db, `users/${auth.currentUser.uid}/items`, id)
+    getDoc(ref)
+      .then((docRef) =>{
+        const tempBodyText = docRef?.data()?.bodyText
+        setBodyText(tempBodyText)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },[])
+  console.log('edit',id)
   return (
-    <KeyboardAvoidingView style={styles.container} behavior='height'>
+    <KeyboardAvoidingView style={styles.container} >
+    {/* iOSで発生するキーボードと投稿ボタンの表示バグ対応の為、KeyboardAvoidingViewは修正版を使用 */}
+
       <View style={styles.inputContainer}>
         {/* multiline iOSで上揃えにする為に必要 */}
-        <TextInput multiline style={styles.input} value={'買いたい物\nリスト'} />
+        <TextInput
+          multiline
+          style={styles.input}
+          value={bodyText}
+          onChangeText={(text) => {setBodyText(text)}}
+          autoFocus
+        />
       </View>
-      <CircleButton onPress={handlePress}>
+      <CircleButton onPress={() => {handlePress(id, bodyText)}}>
         <Icon name='check' size={40} color='#ffffff' />
       </CircleButton>
     </KeyboardAvoidingView>
@@ -31,15 +70,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   inputContainer: {
-    paddingVertical: 32,
-    paddingHorizontal: 27,
     flex: 1
   },
   input: {
     flex: 1,
     textAlignVertical: 'top',
     fontSize: 16,
-    lineHeight: 24
+    lineHeight: 24,
+    paddingVertical: 32,
+    paddingHorizontal: 27
   }
 })
 
