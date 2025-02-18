5d90aed135033cc159d39a1a6bc5cd1b9695a5c5
seriyama <iwakura31@gmail.com>
2025-02-18 20:32:37 +0900

    [編集] refactor: インデント調整

--- src/app/ImpulseBuyStop/edit.tsx
+++ src/app/ImpulseBuyStop/edit.tsx
@@ -1,7 +1,7 @@
-import { 
+import {
     View, TextInput, StyleSheet, KeyboardAvoidingView
 } from 'react-native'
-import { router } from 'expo-router'
+import { router  } from 'expo-router'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/icon'
@@ -13,34 +13,34 @@ const handlePress = (): void => {
 // 編集画面
 
 const Edit = ():JSX.Element => {
-    return (
-        <KeyboardAvoidingView style={styles.container} behavior='height'>
-            <View style={styles.inputContainer}>
-                {/* multiline iOSで上揃えにする為に必要 */}
-                <TextInput multiline style={styles.input} value={'買いたい物\nリスト'} />
-            </View>
-            <CircleButton onPress={handlePress}>
-                <Icon name='check' size={40} color='#ffffff' />
-            </CircleButton>
-        </KeyboardAvoidingView>
-    )
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior='height'>
+      <View style={styles.inputContainer}>
+        {/* multiline iOSで上揃えにする為に必要 */}
+        <TextInput multiline style={styles.input} value={'買いたい物\nリスト'} />
+      </View>
+      <CircleButton onPress={handlePress}>
+        <Icon name='check' size={40} color='#ffffff' />
+      </CircleButton>
+    </KeyboardAvoidingView>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
-    inputContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 27,
-        flex: 1
-    },
-    input: {
-        flex: 1,
-        textAlignVertical: 'top',
-        fontSize: 16,
-        lineHeight: 24
-    }
+  container: {
+    flex: 1
+  },
+  inputContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+    flex: 1
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24
+  }
 })
 
 export default Edit
