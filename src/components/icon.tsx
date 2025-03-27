82b27f70c069990fe42fbf172d8082bb6a0afbd5
seriyama <iwakura31@gmail.com>
2025-03-27 23:12:29 +0900

    [共通] refactro:インデント調整

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -5,30 +5,30 @@ import fontData from '../../assets/fonts/icomoon.ttf'
 import fontSelection from '../../assets/fonts/selection.json'
 
 const CustomIcon = createIconSetFromIcoMoon(
-    fontSelection,
-    'IcoMoon',
-    'icomoon.ttf'
+  fontSelection,
+  'IcoMoon',
+  'icomoon.ttf'
 )
 
 interface Props {
-    name: string
-    size: number
-    color: string
+  name: string
+  size: number
+  color: string
 }
 
 const Icon = (props: Props):JSX.Element | null => {
-    const {name,size,color} = props
-    const [ fontLoaded] = useFonts({
-        IcoMoon: fontData
-    })
+  const {name,size,color} = props
+  const [ fontLoaded] = useFonts({
+      IcoMoon: fontData
+  })
 
-    if(!fontLoaded) {
-        return null
-    }
+  if(!fontLoaded) {
+      return null
+  }
 
-    return (
-        <CustomIcon name={name} size={size} color={color} />
-    )
+  return (
+      <CustomIcon name={name} size={size} color={color} />
+  )
 }
 
 export default Icon
