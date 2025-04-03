18233ee2ef960fa5d9cdb06ea66bf498993d8a76
iwa-ma <iwakura31@gmail.com>
2025-04-03 20:26:03 +0900

    [バグ]fix:ビルドエラー
    config.ts( getReactNativePersistence )のimportを対応前に戻し、警告抑制を復活させた
    icon.tsx( fontData )のパス見直し

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { initializeAuth } from 'firebase/auth'
-import { getReactNativePersistence } from '@firebase/auth/dist/index.rn'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
