dedf2ed2ec877caadd424e151de6159f049f8cb7
iwa-ma <iwakura31@gmail.com>
2025-04-04 16:20:23 +0900

    [リスト]  add : ソート時の初期値を配列→nullに変更
    優先度の取得チェックを削除
    エラー時のデータ設定を追加

--- src/app/ImpulseBuyStop/list.tsx
+++ src/app/ImpulseBuyStop/list.tsx
@@ -147,10 +147,10 @@ const List = ():JSX.Element => {
   useEffect(() => {
     // ログイン中ユーザーが取得でない場合は処理を実行せずに終了する
     if(!auth.currentUser) { return }
-    // 優先度名が取得されていない場合は処理を実行ぜずに終了する
-    if(!state.priorityType) { return }
 
-    dispatch({ type: 'SET_ITEMS', payload: [] })
+    // 依存配列の値が変更されたらitemsをnullにリセット
+    dispatch({ type: 'SET_ITEMS', payload: null })
+
     let collectionPath = ''
     if(anonymous === 'true'){
       // collectionPathにサンプルデータのパスを指定
@@ -199,6 +199,8 @@ const List = ():JSX.Element => {
             'データの取得中にエラーが発生しました。もう一度お試しください。'
           )
         }
+        // エラー発生時はitemsをnullにリセット
+        dispatch({ type: 'SET_ITEMS', payload: [] })
       }
     })
 
